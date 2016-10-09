c0ee8208328dbf006bfd48cb861adb5706913ea1
luoyepugy <1119432095@qq.com>
2016-10-09 19:51:29 +0800

    promise练习完成

--- app/Hello.jsx
+++ app/Hello.jsx
@@ -2,47 +2,88 @@
 import './Hello.css';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 
-
-class User extends React.Component {
+class List extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: '',
-            lastGistUrl: ''
+            loading: true,
+            error: null,
+            data: null
         };
     }
     componentDidMount() {
-        this._isMounted = true;
-        $.get(this.props.source, (result) => {
-            var lastGist = result[0];
-            // console.log(result[0]);
-            if(this._isMounted) {
-                this.setState({
-                    username: lastGist.owner.login,
-                    lastGistUrl: lastGist.html_url
-                });
-            }
-        });
-    }
-    componentWillUnmount() {
-        this._isMounted = false;
+        this.props.promise.then(
+            value => this.setState({loading: false, data: value}),
+            error => this.setState({loading: false, error: error})
+        )
     }
     render() {
-        return (
-            <div>
-                {this.state.username} 's last gist is
-                <a href={this.state.lastGistUrl}>here</a>
-            </div>
-        )
+        if(this.state.loading) {
+            return <span>Loading...</span>
+        } else if(this.state.error != null) {
+            return <span>Error: {this.state.error.message}</span>
+        } else {
+            var repos = this.state.data.items;
+            var repoList = repos.map((repo, index) => {
+                return (
+                    <li key={index}>
+                        <a href={repo.html_url}>{repo.name}</a>({repo.stargazers_count} stars)<br />{repo.descriotion}
+                    </li>
+                )
+            });
+            return (
+                <h1>most popular javascript projects in Github</h1>,
+                <ol>{repoList}</ol>
+            )
+        }
     }
 }
 
 ReactDOM.render(
-    <User source="https://api.github.com/users/octocat/gists" />,
+    <List promise={$.getJSON('https://api.github.com/search/repositories?q=javascript&sort=stars')} />,
     document.getElementById('root')
 );
+
+
+// class User extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             username: '',
+//             lastGistUrl: ''
+//         };
+//     }
+//     componentDidMount() {
+//         this._isMounted = true;
+//         $.get(this.props.source, (result) => {
+//             var lastGist = result[0];
+//             // console.log(result[0]);
+//             if(this._isMounted) {
+//                 this.setState({
+//                     username: lastGist.owner.login,
+//                     lastGistUrl: lastGist.html_url
+//                 });
+//             }
+//         });
+//     }
+//     componentWillUnmount() {
+//         this._isMounted = false;
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 {this.state.username} 's last gist is
+//                 <a href={this.state.lastGistUrl}>here</a>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <User source="https://api.github.com/users/octocat/gists" />,
+//     document.getElementById('root')
+// );
 
 
 // class Hello extends React.Component {
